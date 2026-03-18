07b6ab0ab45d4d1bf0f044db242941f2939849fe
RomanDonw <romandonw628@gmail.com>
2026-03-18 16:37:41 +1000

    pathed not exist memory block alignment in PhysicsHeapAllocator

--- src/engine/physics/PhysicsHeapAllocator.cpp
+++ src/engine/physics/PhysicsHeapAllocator.cpp
@@ -1,9 +1,17 @@
 #include "PhysicsHeapAllocator.hpp"
 
 #include <cstdlib>
+#include <new>
 
 PhysicsHeapAllocator::PhysicsHeapAllocator() : MemoryAllocator() {}
 PhysicsHeapAllocator::~PhysicsHeapAllocator() {}
 
-void *PhysicsHeapAllocator::allocate(size_t size) { return malloc(size); }
-void PhysicsHeapAllocator::release(void *block, size_t size) { free(block); }
+void *PhysicsHeapAllocator::allocate(size_t size)
+{
+    //void *block = std::aligned_alloc(16, size);
+    void *block = _aligned_malloc(size, 16);
+    if (!block) throw std::bad_alloc();
+    return block;
+}
+
+void PhysicsHeapAllocator::release(void *block, size_t size) { _aligned_free(block); }
