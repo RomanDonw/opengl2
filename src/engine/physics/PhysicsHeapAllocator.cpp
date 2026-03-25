c0011d5e5e27726bdbd155fc0ef077ab9d80b112
RomanDonw <romandonw628@gmail.com>
2026-03-25 19:22:03 +1000

    added cross-platform implementation of aligned_alloc/free in PhysicsHeapAllocator class

--- src/engine/physics/PhysicsHeapAllocator.cpp
+++ src/engine/physics/PhysicsHeapAllocator.cpp
@@ -13,9 +13,21 @@ PhysicsHeapAllocator::~PhysicsHeapAllocator() {}
 void *PhysicsHeapAllocator::allocate(size_t size)
 {
     //void *block = std::aligned_alloc(16, size);
-    void *block = _aligned_malloc(size, 16);
+    #ifdef _WIN32
+        void *block = _aligned_malloc(size, 16);
+    #else
+        void *block = std::aligned_alloc(16, size);
+    #endif
+
     if (!block) throw std::bad_alloc();
     return block;
 }
 
-void PhysicsHeapAllocator::release(void *block, size_t size) { _aligned_free(block); }
+void PhysicsHeapAllocator::release(void *block, size_t size)
+{
+    #ifdef _WIN32
+        _aligned_free(block);
+    #else
+        std::free(block);
+    #endif
+}
