076832c09e2d654fb66644e10247623f5a8890d4
RomanDonw <romandonw628@gmail.com>
2026-04-03 21:37:40 +1000

    misc. changed SSBO::BindToSlot method

--- src/engine/render/SSBO.cpp
+++ src/engine/render/SSBO.cpp
@@ -3,7 +3,7 @@
 SSBO::SSBO() { glGenBuffers(1, &buffer); }
 SSBO::~SSBO() { glDeleteBuffers(1, &buffer); }
 
-void SSBO::BindToSlot(GLuint slot) const { glBindBufferBase(GL_SHADER_STORAGE_BUFFER, slot, buffer); }
+void SSBO::BindToSlot(GLuint slot) const { glBindBuffersBase(GL_SHADER_STORAGE_BUFFER, slot, 1, &buffer); }
 
 void SSBO::SetBufferData(void *data, size_t size, GLenum usage)
 {
